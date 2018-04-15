bae4667126b9a4a61acd1f48e634ec18b90a3cd4
Daniil Poplavski <daniilpopl1111@gmail.ocm>
2018-04-15 23:04:01 +0300

    Initial Commit

---
+++ 30 Day UI/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  30 Day UI
+//
+//  Created by Daniil Poplavski on 15.04.2018.
+//  Copyright © 2018 Daniil Poplavski. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
